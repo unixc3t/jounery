1e52f251afd29555b3fec8d09a099ed88ad21f1c
unixc3t <unixc3t@gmail.com>
2018-05-22 13:07:26 +0800

    modiy config

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,13 @@
 <template>
 <div class="header">
-  <div class="header-left">返回</div>
-  <div class="header-input">输入城市/景点/游玩主题</div>
-  <div class="header-right">城市</div>
+  <div class="header-left">
+    <div class="iconfont icon-back">&#xe600;</div>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe612;</span>输入城市/景点/游玩主题</div>
+  <div class="header-right">城市
+    <span class="iconfont arrow-icon">&#xe61c;</span>
+  </div>
 </div>
 </template>
 
@@ -13,15 +18,20 @@ export default {
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-@import '../../../assets/styles/base.scss';
+@import '~styles/base.scss';
+@import '~styles/varibles.scss';
 .header {
   display: flex;
-  background: #00bcd4;
+  background: $bgColor;
   @include x-rem(line-height,.86);
   color: #fff;
   .header-left {
       @include x-rem(width,.64);
       float:left;
+      .icon-back{
+        text-align: center;
+        @include x-rem(font-size,.4)
+      }
   }
   .header-input{
     flex:1;
@@ -29,6 +39,7 @@ export default {
     @include x-rem(height,.62);
     @include x-rem(line-height,.62);
     @include x-rem(margin-left,.2);
+    @include x-rem(padding-left,.2);
     @include x-rem(margin-top,.12);
     @include x-rem(border-radius,.1);
     color: #ccc;
@@ -37,6 +48,10 @@ export default {
     @include x-rem(width,1.24);
     float:right;
     text-align: center;
+    .arrow-icon{
+      @include x-rem(font-size,.2);
+      @include x-rem(margin-left,-.04);
+    }
   }
 }
 </style>
